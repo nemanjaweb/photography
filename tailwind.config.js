5067289fc249091fafa0837ac89a383bb2651307
abc <abc@example.com>
2022-04-25 22:38:49 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      padding: {
+        "160px": "160px",
+      },
+      colors: {
+        "section-3": "#F2F2F2",
+        ffd7a8: "#FFD7A8",
+        c7e8ff: "#C7E8FF",
+        ddbbeff: "#DDBEFF",
+      },
+    },
+  },
+  plugins: [],
+};
